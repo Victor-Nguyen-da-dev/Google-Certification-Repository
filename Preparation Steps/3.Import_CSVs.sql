b189eec1dfcd03296b7e185dd1a4d578b8e43543
Victor <nguyenthang8856@gmail.com>
2025-01-09 08:44:06 +0700

    Proper

--- Preparation Steps/3.Import_CSVs.sql
+++ Preparation Steps/3.Import_CSVs.sql
@@ -10,10 +10,10 @@ COPY dailyIntensities_merged
 FROM 'C:\Test\Github\SQL - Google DA\Data\mturkfitbit_export_4.12.16-5.12.16\Fitabase Data 4.12.16-5.12.16\dailyIntensities_merged.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-COPY heartrate
-FROM 'C:\Test\Github\SQL - Google DA\Data\mturkfitbit_export_4.12.16-5.12.16\Fitabase Data 4.12.16-5.12.16\heartrate.csv'
+COPY heartrate_cleaned
+FROM 'C:\Test\Github\SQL - Google DA\Data\mturkfitbit_export_4.12.16-5.12.16\Fitabase Data 4.12.16-5.12.16\heartrate_cleaned.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-COPY sleepDay
-FROM 'C:\Test\Github\SQL - Google DA\Data\mturkfitbit_export_4.12.16-5.12.16\Fitabase Data 4.12.16-5.12.16\sleepDay.csv'
+COPY sleepDay_cleaned
+FROM 'C:\Test\Github\SQL - Google DA\Data\mturkfitbit_export_4.12.16-5.12.16\Fitabase Data 4.12.16-5.12.16\sleepDay_cleaned.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
